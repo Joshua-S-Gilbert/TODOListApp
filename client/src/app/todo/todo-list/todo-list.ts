b813688aa61510813865055a834f431012f5fdc9
Joshua Gilbert <joshua.gilbert@intravision.com.au>
2025-09-30 14:40:47 +1000

    feat(main page): WIP. comp done, html in progress

---
+++ client/src/app/todo/todo-list/todo-list.ts
@@ -0,0 +1,41 @@
+import { Component, inject, signal } from '@angular/core';
+import { TodoService } from '../todo.service';
+import { TodoItem } from '../todo.model';
+
+@Component({
+  selector: 'app-todo-list',
+  imports: [],
+  templateUrl: './todo-list.html',
+  styleUrl: './todo-list.css',
+  standalone: true  // might be deprecated as of angular 19. angular 18 required it. probably remove in future
+})
+export class TodoList {
+  private api = inject(TodoService);
+  readonly todos = signal<TodoItem[]>([]); 
+  readonly title = signal('');
+
+  constructor(){
+    this.load();
+  }
+
+  load(){
+    this.api.GetAll().subscribe(items => this.todos.set(items));
+  }
+
+  onAdd(){
+    const newTitle = this.title().trim(); // already trim in backend. duplication?
+    if (!newTitle) {
+        return;
+    }
+    this.api.Add(newTitle).subscribe(item => {
+      this.todos.update(list => [item, ...list]);
+      this.title.set('');
+    });
+  }
+
+  onDelete(DeleteId: string){
+    this.api.Delete(DeleteId).subscribe(() => {
+      this.todos.update(list => list.filter(i => i.Id !== DeleteId));
+    });
+  }
+}
