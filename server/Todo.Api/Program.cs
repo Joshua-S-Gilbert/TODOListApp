5da227cdf14b1cbed1400f98580640328c05be52
Joshua Gilbert <joshua.gilbert@intravision.com.au>
2025-09-30 12:30:55 +1000

    fix(program): build errors

--- server/Todo.Api/Program.cs
+++ server/Todo.Api/Program.cs
@@ -7,27 +7,31 @@ using Todo.Core.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer(); // needed for swagger, exposes endpoint metadata
+builder.Services.AddSwaggerGen();   // API documentation
 
-builder.services.AddSingleton<ITodoRepository, InMemoryTodoRepository>();
-builder.services.AddScoped<ITodoService, TodoService>();
+builder.Services.AddSingleton<ITodoRepository, InMemoryTodoRepository>();   // create once, used everywhere until app shutdown. related to stateless service. careful of race conditions on shared memory
+builder.Services.AddScoped<ITodoService, TodoService>();    // 1 instance per request. after request ends, dispose instance
 
 // CORS
-var coresPolicy = "dev";
+var corsPolicy = "dev";
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(coresPolicy, policy => {
-        policy.AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-    })
+    options.AddPolicy(corsPolicy, policy => {
+        policy.AllowAnyOrigin()     // any website can access this API
+                .AllowAnyHeader()   // custom headers allowed
+                .AllowAnyMethod();  // GET, POST, DELETE, etc
+    });
 });
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors(corsPolicy);
+
 app.MapGet("/api/todos", async ([FromServices] ITodoService service, CancellationToken ct) =>{
-    Results.Ok(await service.GetAllAsync(ct));
+    return Results.Ok(await service.GetAllAsync(ct));
 });
 
 app.MapPost("/api/todos", async ([FromServices] ITodoService service, [FromBody] CreateTodo req, CancellationToken ct) => {
@@ -44,12 +48,10 @@ app.MapPost("/api/todos", async ([FromServices] ITodoService service, [FromBody]
 
 app.MapDelete("/api/todos/{id:guid}", async ([FromServices] ITodoService service, Guid id, CancellationToken ct) => {
     var deleted = await service.DeleteAsync(id, ct);
-    return deleted ? Results.NoContent() : Results.NotFound();
+    return deleted ? Results.NoContent() : Results.NotFound();  // 204 vs 404
 });
-
-
 
 
 app.Run();
 
-public record CreateTodo(string title);
+public record CreateTodo(string Title);
